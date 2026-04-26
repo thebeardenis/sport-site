302037b0a3e976e0138b11dc1c2ec19876779f01
denis <denis_2000_kolesnik@mail.ru>
2026-04-26 22:28:26 +0300

    Пока мини изменения, еще ничего нормально не прописал.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+# Домашняя страница для не зарегистрированных пользователей
 @app.route('/')
-def index():
-    return ''
+def home_page_without_auth():
+    return render_template('home_page_without_auth')
 
+# Домашняя страница для зарегистрированных пользователей, получаем id пользователя через переменную пути
+@app.route('/<user_id>')
+def home_page():
+    #Должен быть запрос к бд для получения данных о пользователе
+    return render_template('home_page')
 
 if __name__ == '__main__':
     app.run(debug=True)
